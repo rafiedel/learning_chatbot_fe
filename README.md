d6fac7040df26746e49422c481ced441d9cf0c92
rafiedel <rafie.asadel@ui.ac.id>
2025-06-29 17:26:58 +0700

    docs: add setup command in readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+uses flutter 3.32.0
+
+Run This Command
+```bash
+flutter clean
+flutter pub get
 flutter pub run build_runner build --delete-conflicting-outputs
+```
--- lib/core/conf/conf.g.dart
+++ lib/core/conf/conf.g.dart
@@ -10,7 +10,8 @@ part of 'conf.dart';
 // ignore_for_file: type=lint
 // generated_from: .env
 final class _Conf {
-  static const String baseUrl = 'http://192.168.1.7:8000';
+  static const String baseUrl =
+      'https://learningchatbotbe-production.up.railway.app';
 
   static const String apiKey = 'xxxxxxxxxxxxxxxxxxxxxx';
 }
